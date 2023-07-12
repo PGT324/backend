8f72383d7f5a489b7366a9763c658f48abdc59d7
박건태 <qkrrjsxo456@naver.com>
2023-07-12 16:39:50 +0900

    운동 todo-web 완료

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -23,7 +23,7 @@ const getWorkout = async (req, res) => {
     const workout = await Workout.findById(req.params.id)
 
     if (!workout) {
-        return res.status(404).json({ error: 'No such workout!' })
+        return res.status(404).json(error)
     }
 
     res.status(200).json(workout)
@@ -33,11 +33,25 @@ const getWorkout = async (req, res) => {
 // create new workout
 const createWorkout = async (req, res) => {
     const { title, load, reps } = req.body
+
+    let emptyFields = []
+    if (!title) {
+        emptyFields.push('title')
+    }
+    if (!load) {
+        emptyFields.push('load')
+    }
+    if (!reps) {
+        emptyFields.push('reps')
+    }
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+    }
     try {
         const workout = await Workout.create({ title, load, reps })
         res.status(200).json(workout)
     } catch (error) {
-        res.status(400).json({ error: error.msg })
+        res.status(400).json(error)
     }
 }
 
@@ -49,7 +63,7 @@ const deleteWorkout = async (req, res) => {
         return res.status(404).json({ error: 'No such workout!' })
     }
 
-    res.status(200).json({ msg: 'delete complete!' })
+    res.status(200).json(workout);
 }
 
 // update a workout
